de2dc5d830d13ff6ac81539b02b9123646e5499a
Anton Russinov <anton.russinov@gmail.com>
2020-04-19 18:31:07 +0600

    added function DirReduc

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 
-	go fmt.Println("1!")
-	go fmt.Println("2!")
-	go fmt.Println("3!")
-	go fmt.Println("42112!")
+	result := DirReduc([]string{"SOUT", "WEST", "WEST", "EAST", "NORTH", "SOUTH", "SOUTH", "WEST"})
 
-	fmt.Scanln()
+	fmt.Printf("result of work is %v \n", result)
+}
+
+//DirReduc delete unnecessary directions
+func DirReduc(arr []string) []string {
+
+	for i := 1; i < len(arr); i++ {
+		if arr[i-1] == "NORTH" && arr[i] == "SOUTH" || arr[i-1] == "SOUTH" && arr[i] == "NORTH" {
+			arr = append(arr[:i-1], arr[i+1:]...)
+
+			return DirReduc(arr)
+		} else if arr[i-1] == "EAST" && arr[i] == "WEST" || arr[i-1] == "WEST" && arr[i] == "EAST" {
+			arr = append(arr[:i-1], arr[i+1:]...)
+
+			return DirReduc(arr)
+		}
+	}
+
+	if arr == nil {
+		arr = make([]string, 0, 0)
+	}
+
+	return arr
 }
